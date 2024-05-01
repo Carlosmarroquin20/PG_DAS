315579a342d4174ac16fc776faac2f6ac25d8c34
Emanuel Marroquin <carlosema322@gmail.com>
2024-04-30 23:10:24 -0600

    Manejo inicial de backend,primeras APIs y primer entorno de Admin

--- backend/index.js
+++ backend/index.js
@@ -10,21 +10,126 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// Coneccion de base de datos con MongoDB
+// Conexión de base de datos con MongoDB
 mongoose.connect("mongodb+srv://Ema322:Master322GG@cluster0.j2f0i2y.mongodb.net/DasCommerce");
 
-// Creacion de API
-
+// Creación de API
 app.get("/",(req,res)=>{
-    res.send("La App Express esta corriendo al 100 :)")
+    res.send("La App Express está corriendo al 100 :)")
+});
+
+// Motor para almacenar imágenes
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({storage:storage});
+
+// Crear punto final de carga para imágenes
+app.use('/image', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'),(req,res)=>{
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/image/${req.file.filename}`
+    });
+});
+
+//Shema para la creación de productos
+
+const Product = mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        default:true,
+    },
 })
+
+app.post('/addproduct', async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved")
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+
+})
+
+//ELIMINAR PRODUCTOS
+
+app.post('/removeproduct', async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+
+//API getting all products
+app.get('/allproducts', async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
+
 
 app.listen(port,(error)=>{
     if (!error) {
-        console.log("Server corriendo en puerto: "+port)   
+        console.log("Server corriendo en puerto: "+port);   
     }
     else{
-        console.log("Error : "+error)
+        console.log("Error : "+error);
     }
-
-})
+});
