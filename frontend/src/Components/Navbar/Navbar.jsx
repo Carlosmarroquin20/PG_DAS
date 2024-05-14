4ed44e5c22d27a8da4971dba0ccb8ac50d100c9e
Emanuel Marroquin <carlosema322@gmail.com>
2024-05-13 22:40:53 -0600

    Login y Signup trabajando desde el front al back

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -35,7 +35,9 @@ const Navbar = () => {
                 <li onClick={() => setMenu("Fungicidas")}><Link style={{ textDecoration: 'none' }} to='/fungicidas'>Fungicidas</Link> {menu === "Fungicidas" ? <hr /> : <></>}</li>
             </ul>
             <div className='nav-login-cart'>
-                <Link to='/login'><button>Login</button></Link>
+                {localStorage.getItem('auth-token')
+                ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+                :<Link to='/login'><button>Login</button></Link>} 
                 <Link to='/carrito'><img src={cart_icon} alt="Carrito" /></Link>
                 <div className='nav-cart-count'>{getTotalCartItems()}</div>
             </div>
--- frontend/src/Pages/LoginSingUp.jsx
+++ frontend/src/Pages/LoginSingUp.jsx
@@ -1,24 +1,60 @@
 import React, { useState } from 'react';
 import './CSS/LoginSignup.css';
 
-const LoginSingUp = () => {
+const LoginSignUp = () => {
   const [state, setState] = useState("Inicio de sesión");
-  const [formData,setFormData] = useState({
-    username:"",
-    password:"",
-    email:""
-  })
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: ""
+  });
 
   const changeHandler = (e) => {
-    setFormData({...formData,[e.target.name]:e.target.value})
-  }
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const login = async () => {
-    console.log("Función del Login al 100");
+    console.log("Función del Login al 100", formData);
+    try {
+      const response = await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const responseData = await response.json();
+      if (responseData.success) {
+        localStorage.setItem('auth-token', responseData.token);
+        window.location.replace("/");
+      } else {
+        alert(responseData.errors);
+      }
+    } catch (error) {
+      console.error('Error al realizar el registro:', error);
+    }
   };
 
   const signup = async () => {
-    console.log("Función del SignUp al 100");
+    console.log("Función del SignUp al 100", formData);
+    try {
+      const response = await fetch('http://localhost:4000/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const responseData = await response.json();
+      if (responseData.success) {
+        localStorage.setItem('auth-token', responseData.token);
+        window.location.replace("/");
+      } else {
+        alert(responseData.errors);
+      }
+    } catch (error) {
+      console.error('Error al realizar el registro:', error);
+    }
   };
 
   const handleToggleState = () => {
@@ -30,9 +66,9 @@ const LoginSingUp = () => {
       <div className="loginsignup-container">
         <h1>{state}</h1>
         <div className="loginsignup-fields">
-          {state === "Registrarse" && <input name={formData.username} onChange={changeHandler}  type="text" placeholder='Tu Nombre' />}
-          <input type="email" placeholder='Correo Electrónico' />
-          <input type="password" placeholder='Contraseña' />
+          {state === "Registrarse" && <input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder='Tu Nombre' />}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder='Correo Electrónico' />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder='Contraseña' />
         </div>
         <button onClick={() => (state === "Inicio de sesión" ? login() : signup())}>Continuar</button>
         {state === "Inicio de sesión" 
@@ -47,4 +83,4 @@ const LoginSingUp = () => {
   );
 }
 
-export default LoginSingUp;
+export default LoginSignUp;
